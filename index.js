525192f5ab89251e07ffd85c65de01bc739f247f
mk@Shiva <gangwar3537@gmail.com>
2022-08-12 21:58:33 +0530

    updated README

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const db=require('./config/mongoose');
 const localPort=8000;
 app.use('/',require('./routes'));
-app.listen(localPort,(err)=>{// start server
+app.listen(process.env.PORT || localPort,(err)=>{// start server
     if(err){
         console.log(`Error in running the server: ${err}`);
         return;
